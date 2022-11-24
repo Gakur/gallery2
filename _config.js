e8424b05152a312230c8a2825895e4114dcc6a4d
Gakur <petergakure97@gmail.com>
2022-11-24 11:25:34 +0300

    configure

--- _config.js
+++ _config.js
@@ -4,6 +4,6 @@ var config = {}
 config.mongoURI = {
     production: 'mongodb+srv://Peter1:Mongo@123@dev-ip1.zpsrc3l.mongodb.net/?retryWrites=true&w=majority',
     development: 'mongodb+srv://Peter1:Mongo@123@dev-ip1.zpsrc3l.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://Peter1:Mongo@123@dev-ip1.zpsrc3l.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://Peter1:Mongo@123@dev-ip1.zpsrc3l.mongodb.net/?retryWrites=true&w=majority'
 }
 module.exports = config;
