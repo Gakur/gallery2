f392ec0ff275ba922d6ecfa04a3b6cb45d9955b8
Gakur <petergakure97@gmail.com>
2022-11-24 12:18:46 +0300

    configure new test

--- test/serverTest.js
+++ test/serverTest.js
@@ -13,7 +13,7 @@ describe('Photos', function(){
 
 
     it('should list ALL photos on / GET', function(done){
-        this.timeout(10000);
+        this.timeout(60000);
         chai.request(server)
         .get('/')
         .end(function(err,res){
